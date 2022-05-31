478ebc39fe534b8e9b4f8c6288bcf8069d21a0c3
Teanna Taylor <teannamtaylor95@gmail.com>
2022-05-31 15:18:43 -0700

    commit tree baa7141e5b0502ce1e510f49a91b810f3083d663

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+// assign employee variable to object containing name and street 
+
+
+const employee = {
+    street: '11 Broadway',
+    name: 'Sam'
+}; 
+
+//spread operator
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    const newEmployee = {...employee};
+    newEmployee [key] = value;
+    return newEmployee;
+
+}
+
+//destructive
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+
+    employee[key]=value;
+    return employee;
+}
+
+//spread operator
+function deleteFromEmployeeByKey(employee,key){
+    const newEmployee = {... employee};;
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+//destructive
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key];
+    return employee;
+}
